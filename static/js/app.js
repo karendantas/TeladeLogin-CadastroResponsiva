f80f7859cd29e9c8e85b6a6448a206063bc77969
Karen Dantas <karennobre@gmail.com>
2023-05-24 18:04:16 -0300

    Adicionando responsividade e finalizando

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", ( ) => {
@@ -9,5 +11,15 @@ sign_up_btn.addEventListener("click", ( ) => {
 
 sign_in_btn.addEventListener("click", ( ) => {
     container.classList.remove("sign-up-mode")
+
+});
+
+sign_up_btn2.addEventListener("click", ( ) => {
+    container.classList.add("sign-up-mode2")
+
+});
+
+sign_in_btn2.addEventListener("click", ( ) => {
+    container.classList.remove("sign-up-mode2")
 
 });
